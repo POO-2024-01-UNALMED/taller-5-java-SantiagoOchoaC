8c2745f33014b332d49ddafac49088ff52714020
SantiagoOchoaC <159485469+SantiagoOchoaC@users.noreply.github.com>
2024-03-16 15:22:03 -0500

    Clase Animal

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -1,5 +1,85 @@
 package zooAnimales;
+import gestion.Zona;
 
 public class Animal {
+	private static int totalAnimales = 0;
+	private String nombre;
+	private int edad;
+	private String habitat;
+	private String genero;
+	private Zona zona;
+	
+    // Constructor
+    public Animal(String nombre, int edad, String habitat, String genero) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.habitat = habitat;
+        this.genero = genero;
+        totalAnimales++;
+    }
+    
+    // Constructor Vacio
+    public Animal() {
+    	totalAnimales++; // Deberia contarse
+    }
+    
+    // Métodos
+    public static String totalPorTipo () {
+    	return "Mamiferos: " + Mamifero.cantidadMamiferos() + "\nAves: " + Ave.cantidadAves() + "\nReptiles: " + Reptil.cantidadReptiles() + "\nPeces: " + Pez.cantidadPeces() + "\nAnfibios: " + Anfibio.cantidadAnfibios();
+    }
+    
+    public String toString() {
+    	if (zona != null) {
+    		return "Mi nombre es " + this.getNombre() + ", tengo una edad de " + this.getEdad() + ", habito en " + this.getHabitat() + " y mi genero es " + this.getGenero() + ", la zona en la que me ubico es " + this.zona.getNombre() + ", en el " + this.zona.getZoo();
+    	} else {
+    		return "Mi nombre es " + this.getNombre() + ", tengo una edad de " + this.getEdad() + ", habito en " + this.getHabitat() + " y mi genero es " + this.getGenero();
+    	}
+    	
+    }
+    
+    public String movimiento() {
+    	return "desplazarse";
+    }
+    
+    // Metodos get y set
+    
+    // Métodos para nombre
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    // Métodos para edad
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    // Métodos para habitat
+    public String getHabitat() {
+        return habitat;
+    }
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    // Métodos para genero
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    // Métodos para zona
+    public Zona getZona() {
+        return zona;
+    }
+    public void setZona(Zona zona) {
+        this.zona = zona;
+    }
 }
