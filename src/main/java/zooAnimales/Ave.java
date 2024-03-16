22a6e30df6e261c55a424cd5df9348c5afea5b23
SantiagoOchoaC <159485469+SantiagoOchoaC@users.noreply.github.com>
2024-03-16 15:23:41 -0500

    Clase Ave

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -1,5 +1,59 @@
 package zooAnimales;
+import java.util.ArrayList;
 
-public class Ave {
+public class Ave extends Animal{
+	private static ArrayList<Ave> listado = new ArrayList<>();
+	public static int halcones;
+	public static int aguilas;
+	private String colorPlumas;
+	
+	// Constructor
+	public Ave (String nombre, int edad, String habitat, String genero, String colorPlumas) {
+		super(nombre, edad, habitat, genero);
+		this.colorPlumas = colorPlumas;
+		listado.add(this);
+	}
+	
+	// Constructor Vacio
+	public Ave () {
+		listado.add(this);
+	}
+	
+	// Métodos
+	public static int cantidadAves() {
+		return listado.size();
+	}
+	
+	public static Ave crearHalcon(String nombre, int edad, String genero) {
+		Ave halcon = new Ave(nombre, edad, "montanas", genero, "cafe glorioso");
+		halcones++;
+		return halcon;
+	}
+	
+	public static Ave crearAguila(String nombre, int edad, String genero) {
+		Ave aguila = new Ave(nombre, edad, "montanas", genero, "blanco y amarillo");
+		aguilas++;
+		return aguila;
+	}
+	
+	public String movimiento() {
+		return "volar";
+	}
+	// Métodos get y set
+	
+    // Métodos para listado
+    public ArrayList<Ave> getListado() {
+        return listado;
+    }
+    public void setListado(ArrayList<Ave> listado) {
+        this.listado = listado;
+    }
 
+    // Métodos para colorPlumas
+    public String getColorPlumas() {
+        return colorPlumas;
+    }
+    public void setColorPlumas(String colorPlumas) {
+        this.colorPlumas = colorPlumas;
+    }
 }
